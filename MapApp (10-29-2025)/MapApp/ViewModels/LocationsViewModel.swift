c62b3526664513475b48b56f8547f51253183211
Jackson Harvey <139908928+jaxn-harvey@users.noreply.github.com>
2025-10-29 16:05:41 -0500

    Add files via upload

---
+++ MapApp (10-29-2025)/MapApp/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  LocationsViewModel.swift
+//  MapApp
+//
+//  Created by student on 10/29/25.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+class LocationsViewModel: ObservableObject {
+    
+    // All loaded locations
+    @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        // the line below involves unwrapping which the
+        // swiftful thinking guy has a lot of comments about.
+        // Worth reviewing.
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
+    }
+    
+}
