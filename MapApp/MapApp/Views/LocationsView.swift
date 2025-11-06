2c916dda3e0a526fcda4d4544b99064272364e70
Jackson Harvey <139908928+jaxn-harvey@users.noreply.github.com>
2025-11-06 13:28:39 -0600

    Add files via upload

--- MapApp/MapApp/Views/LocationsView.swift
+++ MapApp/MapApp/Views/LocationsView.swift
@@ -18,6 +18,14 @@ struct LocationsView: View {
     var body: some View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
+            Map(coordinateRegion: $vm.mapRegion,
+                annotationItems: vm.locations,
+                annotationContent: { location in
+                MapAnnotation(coordinate: location.coordinates) {
+                    // we can add our own custom map marker/pin
+                    
+                }
+            })
                 .ignoresSafeArea()
             
             VStack(spacing: 0) {
@@ -25,6 +33,20 @@ struct LocationsView: View {
                     .padding()
                 
                 Spacer()
+                
+                ZStack {
+                    ForEach(vm.locations) { location in
+                        if vm.mapLocation == location {
+                            LocationPreviewView(location: location)
+                                .shadow(color: Color.black.opacity(0.3),
+                                        radius: 20)
+                                .padding()
+                                .transition(.asymmetric(
+                                    insertion: .move(edge: .trailing),
+                                    removal: .move(edge: .leading)))
+                        }
+                    }
+                }
             }
         }
     }
