fdcd46be1a27b369aaf37e8f73a7ca095fee9ba2
Jackson Harvey <139908928+jaxn-harvey@users.noreply.github.com>
2025-11-23 15:30:23 -0600

    Update LocationsDataService.swift

--- MapApp (11-19)/MapApp/DataServices/LocationsDataService.swift
+++ MapApp (11-19)/MapApp/DataServices/LocationsDataService.swift
@@ -14,7 +14,7 @@ class LocationsDataService {
         Location(
             name: "Harrison Plaza Fountain",
             cityName: "Florence",
-            coordinates: CLLocationCoordinate2D(latitude: 34.81, longitude: -87.68),
+            coordinates: CLLocationCoordinate2D(latitude: 34.807201234569064, longitude: -87.67868112097757),
             description: "Dr. Donald and Laura Harrison donated the funds for the fountain which was to be made out of Italian Limestone. The university dedicated the fountain Oct. 21, 2002. On Dec. 18, 2019 the beloved Harrison fountain collapsed after having stood for 17 years. To rebuild the fountain, it was decided to use a marble material which would be carved, crated, and delivered to UNA by ship from Italy and serve as more structurally sound than the previous material. The rebuilding of the fountain completed in 2020.",
             imageNames: [
                 "fountain1",
@@ -25,7 +25,7 @@ class LocationsDataService {
             name: "Guillot University Center",
             cityName: "Florence",
             coordinates: CLLocationCoordinate2D(latitude: 34.8072, longitude:  -87.6803),
-            description: "Also known as the GUC",
+            description: "Named for Robert Miller Guillot, president 1972-1989 the GUC opened in 1988 and contains a variety of offices, including the Student Government Association and the Military & Veteran Service Center. It also contains various student services, a food court, event centers, and social spaces.",
             imageNames: [
                 "GUC1",
                 "GUC2",
@@ -35,7 +35,7 @@ class LocationsDataService {
             name: "East Campus",
             cityName: "Florence",
             coordinates: CLLocationCoordinate2D(latitude: 34.8209, longitude: -87.6589),
-            description: "East Campus",
+            description: "East Campus, set apart from UNA’s main campus, houses the Center for Learning and Professional Development, which offers training and programs for non-traditional students. It is also home to The Joel R. Anderson Center for Innovation and Entrepreneurial Studies and has a student maker space known as the Generator.",
             imageNames: [
                 "eastcampus2",
                 "eastcampus3",
@@ -45,7 +45,7 @@ class LocationsDataService {
             name: "Wesleyan Hall",
             cityName: "Florence",
             coordinates: CLLocationCoordinate2D(latitude: 34.8089, longitude: -87.6791),
-            description: "Wesleyan Hall",
+            description: "Built in 1855, Wesleyan Hall is a piece of Gothic Revival architecture and one of the oldest buildings on campus. Both Confederates and the Union army used this structure in the Civil War. Today it houses the Geography, Foreign Languages, and Psychology departments, and is included in the National Register of Historic Places.",
             imageNames: [
                 "wesleyan1",
                 "wesleyan2",
@@ -55,7 +55,7 @@ class LocationsDataService {
             name: "Norton Auditorium",
             cityName: "Florence",
             coordinates: CLLocationCoordinate2D(latitude: 34.8051, longitude: -87.6803),
-            description: "Norton Auditorium",
+            description: "Norton Auditorium is site to many local ceremonies and performances, including plays, recitals, graduations, and shows by the Shoals Symphony Orchestra. It underwent renovations beginning in 2019 that cost approximately $2 million. It is the second theater in the world that has color-changing lights in its shell.",
             imageNames: [
                 "norton1",
                 "norton2",
